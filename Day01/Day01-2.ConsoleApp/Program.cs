c1310b3d2aa91da89b0f915f10163b955c640a02
johnn01 <john@digifxdesign.com>
2017-12-04 16:23:25 -0600

    Day2 instructions

---
+++ Day01/Day01-2.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day01_2.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
