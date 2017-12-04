2ade92bba93cd893f748275ae60abfd7b88d17e7
johnn01 <john@digifxdesign.com>
2017-12-04 17:21:33 -0600

    Day 2 part 1 complete

---
+++ Day02/Part1.ConsoleApp/Part1.ConsoleApp/DifferenceFinder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Part1.ConsoleApp
+{
+    public class DifferenceFinder
+    {
+        public int GetDifference(string row, string delimiter)
+        {
+            var values = row.Split(delimiter, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x));
+
+            return values.Max() - values.Min();
+        }
+    }
+}
