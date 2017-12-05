7df63161fd86d02ad3eaf09aded4c3aed20bb151
johnn01 <john@digifxdesign.com>
2017-12-05 13:42:20 -0600

    Complete

---
+++ Day02/Part1.ConsoleApp/Part2.ConsoleApp/EvenlyDividesFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Part2.ConsoleApp
+{
+    public class EvenlyDividesFinder
+    {
+        public int Get(IEnumerable<int> row)
+        {
+            var result = row.SelectMany(
+                x => row,
+                (y, z) => { return new { y = y, z = z }; })
+                .Where(x => x.y != x.z
+                    && x.y % x.z == 0)
+                .Select(x => x.y / x.z)
+                .FirstOrDefault();
+
+            //var evenlyDivided = cross.Where(x => x.y % x.z == 0);
+                //.Select(x => x.y / x.z);
+
+            return result;
+        }
+    }
+}
